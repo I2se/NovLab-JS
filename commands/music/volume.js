6b80eb6679ab3d513f058a4c7464fe5409ba5a23
Sothis <81041750+Sothis04@users.noreply.github.com>
2022-02-09 17:02:40 +0100

    Commit

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,20 +1,39 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const { green_light } = require('../../utils/colours.json');
+const { red_light, green_light } = require('../../utils/colours.json');
 const { isInTheSameChannel } = require('../../utils/verif_voc.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('volume')
-        .setDescription('Set the volume'),
+        .setDescription('Set the volume')
+        .addIntegerOption(option => option.setName('percent')
+            .setDescription('Insert the volume of the bot')
+            .setRequired(true)),
     async execute(interaction, message, member, channel, client, guild) {
         const voice_channel = member.voice.channel;
         if (await isInTheSameChannel(voice_channel, client, interaction) == true) {
-            const embed = new MessageEmbed()
-                .setColor(green_light)
-                .setTitle(`Connected to \`\`${voice_channel.name}\`\``)
+            const volume = interaction.options.getInteger('percent');
 
-            interaction.reply({ embeds: [embed] });
+            const queue = client.distube.getQueue(guild.id);
+
+            if (!queue) {
+                interaction.reply({
+                    embeds: [
+                        new MessageEmbed().setColor(red_light)
+                            .setTitle('Volume can\'t change if you don\'t listen music')
+                    ]
+                })
+            } else {
+                client.distube.setVolume(queue, volume);
+
+                interaction.reply({
+                    embeds: [
+                        new MessageEmbed().setColor(green_light)
+                            .setTitle(`The volume is now to \`\`${volume}\`\` %`)
+                    ]
+                })
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./utils/config.json');
+const { token, clientIdSpotify, clientSecretSpotify, cookie } = require('./utils/config.json');
 const { DisTube } = require('distube');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
 const { SpotifyPlugin } = require('@distube/spotify');
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_VOICE_STATES]
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_SCHEDULED_EVENTS]
 });
 
 module.exports = client;
 
+let spotifyoptions = {
+    parallel: true,
+    emitEventsAfterFetching: true,
+}
+
+spotifyoptions.api = {
+    clientId: clientIdSpotify,
+    clientSecret: clientSecretSpotify,
+}
+
 client.distube = new DisTube(client, {
-    emitNewSongOnly: true,
-    nsfw: true,
-    youtubeDL: false,
+    emitNewSongOnly: false,
     leaveOnEmpty: false,
+    leaveOnFinish: false,
     leaveOnStop: false,
-    plugins: [new SpotifyPlugin(), new SoundCloudPlugin()]
+    savePreviousSongs: true,
+    emitAddSongWhenCreatingQueue: false,
+    searchSongs: 0,
+    youtubeCookie: cookie,
+    nsfw: true,
+    emptyCooldown: 25,
+    ytdlOptions: {
+        highWaterMark: 1024 * 1024 * 64,
+        quality: 'highestaudio',
+        format: 'audioonly',
+        liveBuffer: 60000,
+        dlChunkSize: 1024 * 1024 * 4,
+    },
+    youtubeDL: false,
+    updateYouTubeDL: true,
+    plugins: [
+        new SpotifyPlugin(spotifyoptions),
+        new SoundCloudPlugin()
+    ]
 });
 
 ['commands'].forEach(x => client[x] = new Collection());
